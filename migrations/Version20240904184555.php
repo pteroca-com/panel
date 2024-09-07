c67b498c387cdad68a94fc83aa3cbe2e48e9eb30
pteroca-com <pterocacom@gmail.com>
2024-09-08 01:45:54 +0200

    Changed login & register dom titles (#9)
    * Changed login & register document titles
    
    ---------
    
    Co-authored-by: Konrad Sroga <konradsroga@gmail.com>

--- migrations/Version20240904184555.php
+++ migrations/Version20240904184555.php
@@ -20,14 +20,12 @@ final class Version20240904184555 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product CHANGE updated_at updated_at DATETIME NOT NULL, CHANGE category_id category_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE user DROP created_at, DROP updated_at');
+        $this->addSql('ALTER TABLE product CHANGE category_id category_id INT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product CHANGE updated_at updated_at DATETIME DEFAULT NULL, CHANGE category_id category_id INT NOT NULL');
-        $this->addSql('ALTER TABLE user ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE product CHANGE category_id category_id INT NOT NULL');
     }
 }
