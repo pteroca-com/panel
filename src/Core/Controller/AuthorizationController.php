7f10a8330e1e6076fbef3b24080cea668a40e867
Konrad Sroga <konradsroga@gmail.com>
2024-09-07 17:32:20 +0200

    Changed login & register document titles

--- src/Core/Controller/AuthorizationController.php
+++ src/Core/Controller/AuthorizationController.php
@@ -2,8 +2,6 @@
 
 namespace App\Core\Controller;
 
-use App\Core\Enum\SettingEnum;
-use App\Core\Service\SettingService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +9,6 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthorizationController extends AbstractController
 {
-    public function __construct(
-        private readonly SettingService $settingService,
-    ) {}
-
     #[Route(path: '/login', name: 'app_login')]
     public function login(
         AuthenticationUtils $authenticationUtils,
@@ -27,19 +21,9 @@ class AuthorizationController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        $logo = $this->settingService->getSetting(SettingEnum::LOGO->value);
-        $title = $this->settingService->getSetting(SettingEnum::SITE_TITLE->value);
-        if (empty($logo)) {
-            $pageTitle = $title;
-        } else {
-            $logo = sprintf('/uploads/settings/%s', $logo);
-            $pageTitle = sprintf('<img src="%s" alt="%s" class="panel-login-logo" />', $logo, $title);
-        }
-
         return $this->render('login/login.html.twig', [
             'error' => $error,
             'last_username' => $lastUsername,
-            'page_title' => $pageTitle,
             'csrf_token_intention' => 'authenticate',
             'target_path' => $this->generateUrl('panel'),
             'username_parameter' => 'email',
