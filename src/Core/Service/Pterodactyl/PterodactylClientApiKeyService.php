e38903f2e14b127dc1379bd2631921cba86f30da
Konrad Sroga <konradsroga@gmail.com>
2025-01-01 03:12:49 +0100

    [task-97] Updated create client api key service & added synchronize data command (to create client keys for existing users)

--- src/Core/Service/Pterodactyl/PterodactylClientApiKeyService.php
+++ src/Core/Service/Pterodactyl/PterodactylClientApiKeyService.php
@@ -3,27 +3,56 @@
 namespace App\Core\Service\Pterodactyl;
 
 use App\Core\Entity\User;
+use App\Core\Enum\SettingEnum;
+use App\Core\Service\SettingService;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class PterodactylClientApiKeyService
 {
     private const PTERODACTYL_CLIENT_API_KEY_DESCRIPTION = 'PteroCA Client API Key';
 
     public function __construct(
-        private readonly PterodactylService $pterodactylService,
+        private readonly HttpClientInterface $httpClient,
+        private readonly SettingService $settingService,
     )
     {
     }
 
     public function createClientApiKey(User $user): string
     {
-        $createdApiKey = $this->pterodactylService
-            ->getApi()
-            ->users
-            ->http
-            ->post(sprintf('users/%d/api-keys', $user->getPterodactylUserId()), [
-                'description' => self::PTERODACTYL_CLIENT_API_KEY_DESCRIPTION,
-            ]);
+        $endpointUrl = sprintf(
+            '%s/api/application/users/%d/api-keys',
+            $this->settingService->getSetting(SettingEnum::PTERODACTYL_PANEL_URL->value),
+            $user->getPterodactylUserId()
+        );
 
-        return $createdApiKey->get('identifier');
+        $authorizationHeader = sprintf(
+            'Bearer %s',
+            $this->settingService->getSetting(SettingEnum::PTERODACTYL_API_KEY->value)
+        );
+
+        $createdApiKey = $this->httpClient->request('POST', $endpointUrl, [
+            'json' => [
+                'description' => self::PTERODACTYL_CLIENT_API_KEY_DESCRIPTION,
+            ],
+            'headers' => [
+                'Authorization' => $authorizationHeader,
+                'Content-Type' => 'application/json',
+                'Accept' => 'Application/vnd.pterodactyl.v1+json',
+            ],
+        ]);
+
+        $createdApiKey = json_decode(
+            $createdApiKey->getContent(),
+            true,
+            512,
+            JSON_THROW_ON_ERROR
+        );
+
+        return sprintf(
+            '%s%s',
+            $createdApiKey['attributes']['identifier'],
+            $createdApiKey['meta']['secret_token'],
+        );
     }
 }
