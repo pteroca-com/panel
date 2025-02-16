ebbd5f6ea78909aee598cdf1a3630bd77b5e758c
Konrad Sroga <konradsroga@gmail.com>
2025-02-16 15:33:25 +0100

    [v0.4] Added missing translations & changed migration file

--- migrations/Version20250211205333.php
+++ migrations/Version20250211205333.php
@@ -16,8 +16,8 @@ final class Version20250211205333 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('INSERT INTO setting (name, value, type) VALUES (\'pterodactyl_sso_enabled\', \'0\', \'boolean\')');
-        $this->addSql('INSERT INTO setting (name, value, type) VALUES (\'pterodactyl_sso_secret\', \'\', \'secret\')');
+        $this->addSql('INSERT INTO setting (name, value, type, context) VALUES (\'pterodactyl_sso_enabled\', \'0\', \'boolean\', \'general_settings\')');
+        $this->addSql('INSERT INTO setting (name, value, type, context) VALUES (\'pterodactyl_sso_secret\', \'\', \'secret\', \'general_settings\')');
     }
 
     public function down(Schema $schema): void
