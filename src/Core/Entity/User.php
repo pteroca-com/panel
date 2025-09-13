e6209e2806eabed6cf2425296e77455c9b1660fc
Konrad Sroga <konradsroga@gmail.com>
2025-09-14 01:40:14 +0200

    Updated eraseCredentials method

--- src/Core/Entity/User.php
+++ src/Core/Entity/User.php
@@ -289,8 +289,7 @@ class User implements UserInterface
      */
     public function eraseCredentials(): void
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+        $this->plainPassword = null;
     }
 
     public function getDeletedAt(): ?\DateTime
