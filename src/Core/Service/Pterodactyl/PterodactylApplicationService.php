981ba7aa984a822a1d251cf63c95c9613876b6ab
Konrad Sroga <konradsroga@gmail.com>
2025-09-18 20:12:41 +0200

    Improvements

--- src/Core/Service/Pterodactyl/PterodactylApplicationService.php
+++ src/Core/Service/Pterodactyl/PterodactylApplicationService.php
@@ -11,6 +11,10 @@ use App\Core\Service\SettingService;
 
 class PterodactylApplicationService
 {
+    private PterodactylAdapterInterface $applicationCachedAdapter;
+
+    private array $clientCachedAdapters = [];
+
     public function __construct(
         private readonly SettingService $settingService,
         private readonly PterodactylAdapterInterface $pterodactylAdapter,
@@ -20,28 +24,39 @@ class PterodactylApplicationService
 
     public function getApplicationApi(): PterodactylAdapterInterface
     {
+        if (isset($this->applicationCachedAdapter)) {
+            return $this->applicationCachedAdapter;
+        }
+
         $applicationCredentials = new Credentials(
             $this->getApiUrl(),
             $this->settingService->getSetting(SettingEnum::PTERODACTYL_API_KEY->value),
         );
         $this->pterodactylAdapter->setCredentials($applicationCredentials);
+        $this->applicationCachedAdapter = $this->pterodactylAdapter;
 
         return $this->pterodactylAdapter;
     }
 
     public function getClientApi(UserInterface $user): PterodactylClientAdapterInterface
     {
+        if (isset($this->clientCachedAdapters[$user->getId()])) {
+            return $this->clientCachedAdapters[$user->getId()];
+        }
+
         $userCredentials = new Credentials(
             $this->getApiUrl(),
             $user->getPterodactylUserApiKey(),
         );
         $this->pterodactylClientAdapter->setCredentials($userCredentials);
+        $this->clientCachedAdapters[$user->getId()] = $this->pterodactylClientAdapter;
 
         return $this->pterodactylClientAdapter;
     }
 
     private function getApiUrl(): string
     {
-        return $this->settingService->getSetting(SettingEnum::PTERODACTYL_PANEL_URL->value);
+        return $this->settingService
+            ->getSetting(SettingEnum::PTERODACTYL_PANEL_URL->value);
     }
 }
