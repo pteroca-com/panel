1ddd888c02c8ce86508fc1c0c3ef81c5ea714dd4
Konrad Sroga <konradsroga@gmail.com>
2025-11-04 12:48:50 +0100

    Configuration moved to Core bundle

--- src/Core/DependencyInjection/CoreExtension.php
+++ src/Core/DependencyInjection/CoreExtension.php
@@ -10,19 +10,27 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class CoreExtension extends Extension implements PrependExtensionInterface
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
+        $loader->load('plugin_security.yaml');
     }
 
-    public function prepend(ContainerBuilder $container)
+    public function prepend(ContainerBuilder $container): void
     {
+        // Prepend Core bundle configurations
+        $this->prependSecurity($container);
+        $this->prependTwig($container);
+        $this->prependTranslation($container);
+        $this->prependMonolog($container);
+        $this->prependVichUploader($container);
+
         // Add plugin translation paths to framework translation configuration
         $this->prependPluginTranslations($container);
     }
 
-    private function prependPluginTranslations(ContainerBuilder $container)
+    private function prependPluginTranslations(ContainerBuilder $container): void
     {
         $projectDir = $container->getParameter('kernel.project_dir');
         $pluginsDir = $projectDir . '/plugins';
@@ -81,6 +89,78 @@ class CoreExtension extends Extension implements PrependExtensionInterface
                     'paths' => $translationPaths,
                 ],
             ]);
+        }
+    }
+
+    private function prependSecurity(ContainerBuilder $container): void
+    {
+        $configFile = __DIR__ . '/../Resources/config/security.yaml';
+        if (!file_exists($configFile)) {
+            return;
+        }
+
+        $config = \Symfony\Component\Yaml\Yaml::parseFile($configFile);
+        if (isset($config['security'])) {
+            $container->prependExtensionConfig('security', $config['security']);
+        }
+    }
+
+    private function prependTwig(ContainerBuilder $container): void
+    {
+        $configFile = __DIR__ . '/../Resources/config/twig.yaml';
+        if (!file_exists($configFile)) {
+            return;
+        }
+
+        $config = \Symfony\Component\Yaml\Yaml::parseFile($configFile);
+        if (isset($config['twig'])) {
+            $container->prependExtensionConfig('twig', $config['twig']);
+        }
+    }
+
+    private function prependTranslation(ContainerBuilder $container): void
+    {
+        $configFile = __DIR__ . '/../Resources/config/translation.yaml';
+        if (!file_exists($configFile)) {
+            return;
+        }
+
+        $config = \Symfony\Component\Yaml\Yaml::parseFile($configFile);
+        if (isset($config['framework'])) {
+            $container->prependExtensionConfig('framework', $config['framework']);
+        }
+    }
+
+    private function prependMonolog(ContainerBuilder $container): void
+    {
+        $configFile = __DIR__ . '/../Resources/config/monolog.yaml';
+        if (!file_exists($configFile)) {
+            return;
+        }
+
+        $config = \Symfony\Component\Yaml\Yaml::parseFile($configFile);
+        if (isset($config['monolog'])) {
+            $container->prependExtensionConfig('monolog', $config['monolog']);
+        }
+
+        // Prepend environment-specific configs
+        $environment = $container->getParameter('kernel.environment');
+        $whenKey = 'when@' . $environment;
+        if (isset($config[$whenKey]['monolog'])) {
+            $container->prependExtensionConfig('monolog', $config[$whenKey]['monolog']);
+        }
+    }
+
+    private function prependVichUploader(ContainerBuilder $container): void
+    {
+        $configFile = __DIR__ . '/../Resources/config/vich_uploader.yaml';
+        if (!file_exists($configFile)) {
+            return;
+        }
+
+        $config = \Symfony\Component\Yaml\Yaml::parseFile($configFile);
+        if (isset($config['vich_uploader'])) {
+            $container->prependExtensionConfig('vich_uploader', $config['vich_uploader']);
         }
     }
 }
